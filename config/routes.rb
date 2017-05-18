d9c97b2e993928af3b8957f36055c819755795e5
plupinska <patrycjalupinska@gmail.com>
2017-05-18 16:08:19 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  root "static_pages#root"
+  root "static_pages#index"
 
   namespace :api, defaults: {format: :json} do
     resources :bots, only: [:create, :index]
   end
-  
+
 end
